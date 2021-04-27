bca4862a8df3e9ab63dad77fd060cadb4f2e6a43
Jiří Skalský <j.skalsky@centrum.cz>
2021-04-27 19:34:03 +0200

    Prvni verze

---
+++ FileManager/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace FileManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            string appFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + Path.DirectorySeparatorChar + "Gis";
+            if (!Directory.Exists(appFolder)) Directory.CreateDirectory(appFolder);
+#if DEBUG
+            FileStream myTraceLog = new FileStream(appFolder + System.IO.Path.DirectorySeparatorChar + "Gis.deb", FileMode.Create);
+            TextWriterTraceListener myListener = new TextWriterTraceListener(myTraceLog);
+            Debug.Listeners.Add(myListener);
+            Debug.AutoFlush = true;
+            Debug.WriteLine("Start");
+#endif
+            Dispatcher.UnhandledException += Dispatcher_UnhandledException;
+        }
+
+        private void Dispatcher_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string msg = $"Exception: {e.Exception.Message}";
+            MessageBox.Show(msg, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            Debug.WriteLine($"Exception: {e.Exception.Message}");
+            if (e.Exception.InnerException != null)
+            {
+                Debug.Print($"Inner exception: {e.Exception.InnerException.Message}");
+            }
+            StackTrace stackTrace = new StackTrace(e.Exception, true);
+            for (int i = 0; i < stackTrace.FrameCount; ++i)
+            {
+                Debug.WriteLine($"  {stackTrace.GetFrame(i).GetFileName()}, {stackTrace.GetFrame(i).GetFileLineNumber()} : {stackTrace.GetFrame(i).GetMethod().Name}");
+            }
+            e.Handled = false;
+        }
+    }
+}
