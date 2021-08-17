35d2b62fe916d4c696228dbbcbe6a38d2e49cb4b
Jiří Skalský <j.skalsky@centrum.cz>
2021-08-17 20:15:39 +0200

    ExcelReader

---
+++ OpcSrcMaker/ExcelReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace OpcSrcMaker
+{
+    public class ExcelReader
+    {
+        Excel.Application excelApp = new Excel.Application();
+    }
+}
